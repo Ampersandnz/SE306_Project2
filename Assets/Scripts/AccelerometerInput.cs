7cf6761dd8ffc14719e17b6d2f318d68c381592a
shiverz01 <shivvy1@hotmail.com>
2014-10-03 10:47:27 +1300

    Accelerometer script

---
+++ Assets/Scripts/AccelerometerInput.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour {
+
+	// Read accelerometer input in the x direction
+	void Update () {
+		transform.Translate (Input.acceleration.x, 0, 0);
+	}
+}
