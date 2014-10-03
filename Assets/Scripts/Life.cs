2a5fa6e8080663e9503caec7628590ca36deea1d
Brian Cham <king__lego@hotmail.com>
2014-10-03 22:23:33 +1300

    Adding resource folder

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+
+	private PlayerStory player;
+
+	void Start(){	
+		player = (PlayerStory)FindObjectOfType (typeof(PlayerStory));
+	}
+
+	// If the player is at maximum health, make the health pack disappear.
+	void Update(){
+		if(player.health>=player.max_health){
+			Destroy (gameObject);
+		}
+	}
+
+	// Detect all collisions
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.transform.gameObject.name == "Swiper") {
+			if( player.health < player.max_health){
+				Destroy (gameObject); // If collision is with Swiper, destroy the life immediately.
+			}
+		}
+	}
+}
