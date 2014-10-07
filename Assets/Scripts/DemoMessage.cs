c10fa6e3479b47ca7f374ce893918c932fd8bb8d
Brian Cham <king__lego@hotmail.com>
2014-10-07 16:19:56 +1300

    Restoring scene that was removed by accident

---
+++ Assets/Scripts/DemoMessage.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Class to display the demo message.
+public class DemoMessage : MonoBehaviour {
+	private SoundPlayer soundPlayer;
+	public GUIStyle textStyleTitle;
+	public GUIStyle textStyleButton;
+	public GUISkin skin;
+	public string musicSymbol;
+	
+	// Initialise the sound player.
+	void Start(){
+		soundPlayer = FindObjectOfType(typeof(SoundPlayer)) as SoundPlayer;
+		DontDestroyOnLoad (soundPlayer);
+		
+		textStyleTitle.fontSize = Screen.height / 50 * 4;
+		textStyleButton.fontSize = Screen.height / 50 * 3;
+	}
+	
+	void OnGUI () {
+		GUI.skin = skin;
+		
+		// Title
+		GUI.Label (new Rect (Screen.width / 2 - Screen.width/4, Screen.height/20, Screen.width/2, Screen.height/20*3), "This feature not in demo yet", textStyleTitle);
+		
+		// Button to go back to endless runner mode menu.
+		if(GUI.Button(new Rect(Screen.width/2-Screen.width/6, Screen.height/20*3 + Screen.height/5 , Screen.width/3, Screen.height/20*3), "Main menu", textStyleButton)) {
+			Application.LoadLevel("MainMenu");
+			soundPlayer.PlaySoundEffect ("menu");
+		}
+		
+		// Button to toggle music
+		if(GUI.Button(new Rect(Screen.width-Screen.width/24*5,Screen.height/30, Screen.width/12 , Screen.height/20*3), musicSymbol, textStyleButton)) {
+			soundPlayer.PlaySoundEffect ("menu");
+			soundPlayer.ToggleMusic("menu");
+		}
+		
+		// Button to toggle sound effects
+		if(GUI.Button(new Rect(Screen.width-Screen.width/60*7, Screen.height/30, Screen.width/120*11, Screen.height/20*3), "sfx", textStyleButton)) {
+			soundPlayer.PlaySoundEffect ("menu");
+			soundPlayer.ToggleSound();
+		}
+	}
+}
