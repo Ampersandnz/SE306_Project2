d595ca81b98e49f192d7dabfbde5569cb3f3401d
richap93 <rpat789@aucklanduni.ac.nz>
2014-10-11 23:44:15 +1300

    fixed camera movement offset problem

--- Assets/Scripts/CameraTracksSwiper.cs
+++ Assets/Scripts/CameraTracksSwiper.cs
@@ -7,9 +7,6 @@ public class CameraTracksSwiper : MonoBehaviour {
 	float offset;
 	PlayerStory player_GO;
 	private Vector3 prev_pos;
-	private Vector3 orig_pos;
-	public bool move_back;
-
 
 	// Use this for initialization
 	void Start () {
@@ -20,30 +17,21 @@ public class CameraTracksSwiper : MonoBehaviour {
 			return;
 		}
 
-		move_back = true;
 		player = player_GO.transform;
 		prev_pos = player.position;
-		orig_pos = player.position;
 		offset = transform.position.x - player.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (player != null && !player_GO.playerDead) {
-//			if (player.position.x < prev_pos.x) {
-//				move_back = false;
-//			}
-//			if (player.position.x > prev_pos.x) {
-//				move_back = true;
-			//			} && (Mathf.Approximately(diff, offset))
 			float diff = transform.position.x - player.position.x;
-			if (player.position.x > prev_pos.x) {
+			if (player.position.x > prev_pos.x && diff < offset) {
 				Vector3 pos = transform.position;
 				pos.x = player.position.x + offset;
 				transform.position = pos;
 			}
 			prev_pos = player.position;
-
 		}
 	}
 	
