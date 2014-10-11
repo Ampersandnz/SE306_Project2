2c78e7f8221d4b77f34cd3d0c1f45ca6ab4cdc74
richap93 <rpat789@aucklanduni.ac.nz>
2014-10-11 23:41:26 +1300

    Camera only moves forward now

--- Assets/Scripts/CameraTracksSwiper.cs
+++ Assets/Scripts/CameraTracksSwiper.cs
@@ -7,8 +7,10 @@ public class CameraTracksSwiper : MonoBehaviour {
 	float offset;
 	PlayerStory player_GO;
 	private Vector3 prev_pos;
+	private Vector3 orig_pos;
+	public bool move_back;
 
-	
+
 	// Use this for initialization
 	void Start () {
 		player_GO = FindObjectOfType (typeof(PlayerStory)) as PlayerStory;
@@ -17,15 +19,24 @@ public class CameraTracksSwiper : MonoBehaviour {
 			Debug.LogError ("No player!");
 			return;
 		}
-		
+
+		move_back = true;
 		player = player_GO.transform;
 		prev_pos = player.position;
+		orig_pos = player.position;
 		offset = transform.position.x - player.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (player != null && !player_GO.playerDead) {
+//			if (player.position.x < prev_pos.x) {
+//				move_back = false;
+//			}
+//			if (player.position.x > prev_pos.x) {
+//				move_back = true;
+			//			} && (Mathf.Approximately(diff, offset))
+			float diff = transform.position.x - player.position.x;
 			if (player.position.x > prev_pos.x) {
 				Vector3 pos = transform.position;
 				pos.x = player.position.x + offset;
@@ -35,4 +46,5 @@ public class CameraTracksSwiper : MonoBehaviour {
 
 		}
 	}
+	
 }
