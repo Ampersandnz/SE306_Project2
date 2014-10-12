6634c5a7dcadd2129d6c67fe23e7a34d951f8e3c
lwan353 <lwan353@aucklanduni.ac.nz>
2014-10-12 15:18:31 +1300

    red ant change into black ant when hit form top

--- Assets/Scripts/RedAnt.cs
+++ Assets/Scripts/RedAnt.cs
@@ -7,13 +7,13 @@ public class RedAnt : MonoBehaviour {
 	public float rightLimit;
 	public int direction; // Direction of movement. 1 is left and -1 is right.
 	
-	//public bool alive;
+	public bool alive;
 	private SoundPlayer soundPlayer;
 	Animator anim;
 	
 	void Start () {
-	//	alive = true;
-	//	anim = GetComponent<Animator> ();
+		alive = true;
+		anim = GetComponent<Animator> ();
 		
 		// Getting reference to player object.
 		player = FindObjectOfType(typeof(PlayerStory)) as PlayerStory;
@@ -30,25 +30,35 @@ public class RedAnt : MonoBehaviour {
 	
 	void Update () {
 		// If ant is dead, then:
-		//if (alive == false) {
-		//	anim.SetBool ("isAlive", false); // Change to "dead ant" texture.
-		//	collider2D.enabled = false; // Make ant intangible so Swiper can't collide with the carcass.
-		//} else {
+		if (alive == false) {
+			anim.SetBool ("hit1", true); // Change to "dead ant" texture.
+
+			//transform.localScale = new Vector2(0.3f , 0.3f);
+			if (transform.position.x > rightLimit) {
+				direction = 1;
+				transform.localScale = new Vector2(0.3f , 0.3f); // Flip sprite horizontally
+			} else if (transform.position.x < leftLimit) {
+				direction = -1;
+				transform.localScale = new Vector2(-0.3f , 0.3f); // Flip sprite horizontally
+			}
+			transform.position = new Vector2 (transform.position.x + (-0.02f * (float)direction), transform.position.y);
+			collider2D.enabled = false; // Make ant intangible so Swiper can't collide with the carcass.
+		} else {
 			
 			// Moving left and right
-		if (transform.position.x > rightLimit) {
-			direction = 1;
-			transform.localScale = new Vector2(0.6f , 0.6f); // Flip sprite horizontally
-		} else if (transform.position.x < leftLimit) {
-			direction = -1;
-			transform.localScale = new Vector2(-0.6f , 0.6f); // Flip sprite horizontally
-		}
-		transform.position = new Vector2 (transform.position.x + (-0.02f * (float)direction), transform.position.y);
+			if (transform.position.x > rightLimit) {
+				direction = 1;
+				transform.localScale = new Vector2(0.6f , 0.6f); // Flip sprite horizontally
+			} else if (transform.position.x < leftLimit) {
+				direction = -1;
+				transform.localScale = new Vector2(-0.6f , 0.6f); // Flip sprite horizontally
+			}
+			transform.position = new Vector2 (transform.position.x + (-0.02f * (float)direction), transform.position.y);
 			
-		//}
+		}
 	}
 	
-	/*void OnCollisionEnter2D(Collision2D other) {
+	void OnCollisionEnter2D(Collision2D other) {
 		
 		// If collision is with Swiper, check if it is dead.
 		if (other.transform.gameObject.name == "Swiper") {
@@ -57,5 +67,5 @@ public class RedAnt : MonoBehaviour {
 				soundPlayer.PlaySoundEffect("crunch");
 			}
 		}
-	}*/
+	}
 }
