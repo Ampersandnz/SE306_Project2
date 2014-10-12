1edf6b94dfdbb4c6d9f6d0ff1adf00c58bf63566
richap93 <rpat789@aucklanduni.ac.nz>
2014-10-12 19:24:13 +1300

    Added end of level one scene with a background

---
+++ Assets/Scripts/EndOfLevelOne.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndOfLevelOne : MonoBehaviour {
+
+	public GUIStyle textStyleButton;
+	public GUISkin skin;
+
+	// Use this for initialization
+	void Start () {
+		textStyleButton.fontSize = Screen.height / 50 * 3;
+
+	}
+	
+	// Displaying everything
+	void OnGUI () {
+		GUI.skin = skin;
+		
+		// Button to go back to main menu
+		if(GUI.Button(new Rect(Screen.width/8*7, Screen.height/20*12 + Screen.height/10*3, Screen.width/3, Screen.height/20*3), "Back", textStyleButton)) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
