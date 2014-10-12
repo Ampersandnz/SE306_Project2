a4c70bfbc4d9956045e6e0f68b7b46e80d322ee3
lwan353 <lwan353@aucklanduni.ac.nz>
2014-10-12 15:49:22 +1300

    ant dead after second hit

--- Assets/Scripts/RedAnt.cs
+++ Assets/Scripts/RedAnt.cs
@@ -8,6 +8,8 @@ public class RedAnt : MonoBehaviour {
 	public int direction; // Direction of movement. 1 is left and -1 is right.
 	
 	public bool alive;
+	public bool Hit1 = false;
+	public int hitCount;
 	private SoundPlayer soundPlayer;
 	Animator anim;
 	
@@ -29,8 +31,8 @@ public class RedAnt : MonoBehaviour {
 	}
 	
 	void Update () {
-		// If ant is dead, then:
-		if (alive == false) {
+		// If red ant lose one live, then:
+		if (Hit1 == true) {
 			anim.SetBool ("hit1", true); // Change to "dead ant" texture.
 
 			//transform.localScale = new Vector2(0.3f , 0.3f);
@@ -42,8 +44,22 @@ public class RedAnt : MonoBehaviour {
 				transform.localScale = new Vector2(-0.3f , 0.3f); // Flip sprite horizontally
 			}
 			transform.position = new Vector2 (transform.position.x + (-0.02f * (float)direction), transform.position.y);
-			collider2D.enabled = false; // Make ant intangible so Swiper can't collide with the carcass.
-		} else {
+			//collider2D.enabled = false; // Make ant intangible so Swiper can't collide with the carcass.
+			//Hit1 = false;
+
+		} 
+
+		// if ant dead
+		else if(alive == false){
+
+			anim.SetBool ("hit2", true); // Change to "dead ant" texture.
+			collider2D.enabled = false;
+			Destroy (gameObject, 0.5f);
+
+
+		}
+
+		else {
 			
 			// Moving left and right
 			if (transform.position.x > rightLimit) {
@@ -63,8 +79,19 @@ public class RedAnt : MonoBehaviour {
 		// If collision is with Swiper, check if it is dead.
 		if (other.transform.gameObject.name == "Swiper") {
 			if (player.transform.position.y - 0.6f >= transform.position.y + 0.7) {
-				alive = false;
-				soundPlayer.PlaySoundEffect("crunch");
+				hitCount++;
+
+				if(hitCount == 1){
+					Hit1 = true;
+				}
+
+				if (hitCount == 2){
+					Hit1 = false;
+					alive = false;
+					hitCount = 0;
+					soundPlayer.PlaySoundEffect("crunch");
+				}
+
 			}
 		}
 	}
