d289c5b1353abe96094e8d06c6b7ea0b531fd357
nikita22 <nikita-nikita22@hotmail.com>
2014-10-13 22:13:57 +1300

    Layered Background properly

--- Assets/Scripts/CameraTracksSwiper.cs
+++ Assets/Scripts/CameraTracksSwiper.cs
@@ -5,7 +5,6 @@ public class CameraTracksSwiper : MonoBehaviour {
 	
 	Transform player;
 	float offset;
-	float offset_y;
 	PlayerStory player_GO;
 	
 	// Use this for initialization
@@ -19,7 +18,7 @@ public class CameraTracksSwiper : MonoBehaviour {
 		
 		player = player_GO.transform;
 		offset = transform.position.x - player.position.x;
-		offset_y = transform.position.y - player.position.y;
+		offset = transform.position.y - player.position.y;
 	}
 	
 	// Update is called once per frame
@@ -27,9 +26,7 @@ public class CameraTracksSwiper : MonoBehaviour {
 		if (player != null && !player_GO.playerDead) {
 			Vector3 pos = transform.position;
 			pos.x = player.position.x + offset;
-			if (player.position.y > 3f){
-				pos.y = player.position.y + offset_y - 2f;
-			}
+			pos.y = player.position.y + offset;
 			transform.position = pos;
 		}
 	}
