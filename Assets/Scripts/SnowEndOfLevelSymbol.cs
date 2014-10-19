2dbf5f0fb4b6bfab06d7f5e89e39cb3625367bd3
richap93 <rpat789@aucklanduni.ac.nz>
2014-10-19 14:16:29 +1300

    Fixed restart level at end of level 2 and 3

--- Assets/Scripts/SnowEndOfLevelSymbol.cs
+++ Assets/Scripts/SnowEndOfLevelSymbol.cs
@@ -16,7 +16,7 @@ public class SnowEndOfLevelSymbol : MonoBehaviour {
 	void OnMouseDown(){
 		// this object was clicked - do something
 		Application.LoadLevel("StoryLevel3");	
-		StoryLevelSelect.currentLevel = 2; 
+		StoryLevelSelect.currentLevel = 3; 
 
 	}
 }
