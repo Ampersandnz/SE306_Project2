57b074754ca18987366ab9c5d5b234a43815847b
richap93 <rpat789@aucklanduni.ac.nz>
2014-10-19 15:07:44 +1300

    Added start of endless runner scene

---
+++ Assets/Scripts/StartOfEndless.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartOfEndless : MonoBehaviour {
+
+	public GUIStyle textStyleButton;
+	public GUISkin skin;
+	public GUIStyle textStyleTitle;
+	
+	// Use this for initialization
+	void Start () {
+		textStyleButton.fontSize = Screen.height / 50 * 3;
+		textStyleTitle.fontSize = Screen.height / 50 * 4;
+		
+	}
+	
+	// Displaying everything
+	void OnGUI () {
+		GUI.skin = skin;
+		
+		// Button to go back to main menu
+		if(GUI.Button(new Rect(Screen.width/30*25, Screen.height/20*11 + Screen.height/10*3, Screen.width/6, Screen.height/20*3), "Next", textStyleButton)) {
+			Application.LoadLevel("Level3End2");
+		}
+	}
+}
