7e2a0b4fa781e7111362a3514b9d79fef9a56e86
lwan353 <lwan353@aucklanduni.ac.nz>
2014-10-21 23:07:08 +1300

    Spider not generate coin anymore after it dead

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -19,25 +19,8 @@ public class Spider : MonoBehaviour {
 	void Update () {
 	}
 
-	/* Method to create an object. Paramaters:
-	 * 	- obj - The GameObject to clone.
-	 * 	- x - The absolute x ordinate of the object.
-	 * 	- y - The absolute y ordinate of the object.
-	*/
-	public void CreateObject(GameObject obj, float x, float y){
-		Instantiate (obj, new Vector2 (x, y), Quaternion.identity);
-	}
-
 	public void Die() {
 		alive = false;
-
-		// the position of ant's before it dead
-		var positionX = transform.position.x;
-		var positionY = transform.position.y;
-		
-		//generate a coin when the ant been killed.
-		CreateObject (coin, positionX+1.2f, positionY+3.0f);
-
 		soundPlayer.PlaySoundEffect ("crunch");
 		Destroy (gameObject, 0.5f);
 	}
