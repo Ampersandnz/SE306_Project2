c4c34c163b138ed1fc550b6f4f6060ef24e86648
lwan353 <lwan353@aucklanduni.ac.nz>
2014-10-21 23:45:59 +1300

    change enemy collision for endless

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -51,9 +51,21 @@ public class EnemyCollision : MonoBehaviour {
 		
 		Spider spider = other.gameObject.GetComponent<Spider>();
 		if (spider != null && spider.alive) {
-			spider.Die();
-			playerStory.rigidbody2D.AddForce(playerStory.enemyBounce);
-			Physics2D.IgnoreCollision(other, playerStory.collider2D, true);
+
+			//the lowest position of swiper's collider box
+			var colliderSwiper = GetComponent<EdgeCollider2D>();
+			var colliderSw = colliderSwiper.collider2D;
+			
+			// the highest position of Spider's collider
+			var colliderSpider = spider.GetComponent<BoxCollider2D>();
+			var colliderSp = colliderSpider.collider2D;
+
+			if (colliderSw.bounds.min.y + 0.2f >= colliderSp.bounds.max.y) {
+				spider.Die();
+				playerStory.rigidbody2D.AddForce(playerStory.enemyBounce);
+				Physics2D.IgnoreCollision(other, playerStory.collider2D, true);
+			}
+
 		}
 	}
 }
--- Assets/Scripts/EnemyCollisionEndless.cs
+++ Assets/Scripts/EnemyCollisionEndless.cs
@@ -10,16 +10,41 @@ public class EnemyCollisionEndless : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other) {
 		AntEndless ant = other.gameObject.GetComponent<AntEndless>();
 		if (ant != null && ant.alive) {
-			ant.Die ();
-			playerStory.rigidbody2D.AddForce(playerStory.enemyBounce);
-			Physics2D.IgnoreCollision(other, playerStory.collider2D, true);
+
+			//the lowest position of swiper's collider box
+			var colliderSwiper = GetComponent<EdgeCollider2D>();
+			var colliderS = colliderSwiper.collider2D;
+			
+			// the highest position of Ant's collider
+			var colliderAnt = ant.GetComponent<BoxCollider2D>();
+			var colliderA = colliderAnt.collider2D;
+			
+			print("S"+colliderS.bounds.min.y);
+			print ("A"+colliderA.bounds.max.y);
+			if (colliderS.bounds.min.y + 0.2f >= colliderA.bounds.max.y) {
+				ant.Die ();
+				playerStory.rigidbody2D.AddForce(playerStory.enemyBounce);
+				Physics2D.IgnoreCollision(other, playerStory.collider2D, true);
+			}
 		}
 
 		Spider spider = other.gameObject.GetComponent<Spider>();
 		if (spider != null && spider.alive) {
-			spider.Die();
-			playerStory.rigidbody2D.AddForce(playerStory.enemyBounce);
-			Physics2D.IgnoreCollision(other, playerStory.collider2D, true);
+
+			//the lowest position of swiper's collider box
+			var colliderSwiper = GetComponent<EdgeCollider2D>();
+			var colliderSw = colliderSwiper.collider2D;
+			
+			// the highest position of Spider's collider
+			var colliderSpider = spider.GetComponent<BoxCollider2D>();
+			var colliderSp = colliderSpider.collider2D;
+			
+			if (colliderSw.bounds.min.y + 0.2f >= colliderSp.bounds.max.y) {
+				spider.Die();
+				playerStory.rigidbody2D.AddForce(playerStory.enemyBounce);
+				Physics2D.IgnoreCollision(other, playerStory.collider2D, true);
+			}
+
 		}
 	}
 }
